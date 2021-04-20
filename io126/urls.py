e04ce47b48d1248bbb84948cb2d4395e86bb62ee
salahuya <1185456367@qq.com>
2021-04-20 23:05:32 +0800

    修个bug

--- io126/urls.py
+++ io126/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path,include
 from io126 import views
 
-urlpatterns = [path('in_126/',views.show,name='in_126'),]
+urlpatterns = [path('in_126/',views.in_126,name='in_126'),]
